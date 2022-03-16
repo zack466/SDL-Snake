7e1b457f3744335fb6705d9765eae00ceaad5c79
Zachary Huang <zack466@gmail.com>
2022-03-15 22:34:00 -0400

    edit readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Build and Run
 
-Make sure `sdl2` and `sdl2_ttf` are installed.
+Make sure `cmake`, `pkgconfig`, `sdl2` and `sdl2_ttf` are installed.
 
 ```
 git clone https://github.com/zack466/SDL-Snake
@@ -22,3 +22,8 @@ make run
 
 - `wasd` or arrow keys to control the snake
 - `q` to quit.
+
+## Config
+
+You can change certain settings through the `src/config.h` file.
+Just edit with your favorite editor, and re-run `make run`.
--- src/config.h
+++ src/config.h
@@ -1,5 +1,8 @@
+// height and width of the opened window in pixels
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
+
+// the side length, in pixels, of each grid pixel
 #define PIXEL_SIZE 40
 
 // how much time in between each frame
@@ -8,7 +11,7 @@
 // how many frames it takes for the snake to advance
 #define SNAKE_SPEED 8
 
+// don't edit these
 #define HEIGHT WINDOW_HEIGHT / PIXEL_SIZE
 #define WIDTH WINDOW_WIDTH / PIXEL_SIZE
-
 #define NUM_PIXELS (HEIGHT * WIDTH)
